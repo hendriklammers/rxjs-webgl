38191c818482bf3452a04b1651d5ef37557c5181
Hendrik Lammers <lammers@gmail.com>
2016-06-05 12:10:12 +0200

    Init canvas

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
-console.log('bla')
+import {Observable} from 'rxjs/Rx'
+
+function createCanvas() {
+  const canvas = document.createElement('canvas')
+  canvas.style.width = '100vw'
+  canvas.style.height = '100vh'
+
+  return canvas
+}
+
+function getBody() {
+  const body = document.body
+  body.style.margin = '0'
+  body.style.padding = '0'
+  body.style.overflow = 'hidden'
+
+  return body
+}
+
+function init() {
+  const body = getBody(),
+    canvas = createCanvas()
+
+  body.appendChild(canvas)
+}
+
+Observable
+  .fromEvent(document, 'DOMContentLoaded')
+  .subscribe(init)
