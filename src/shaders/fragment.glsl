0f2e3f83b35fb428e6ecd528e3ed977e02c903ec
Hendrik Lammers <lammers@gmail.com>
2016-08-09 23:21:59 +0200

    Replace colors

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -16,24 +16,17 @@ void rect(vec2 coord, vec2 pos, vec2 dimensions, vec3 color, inout vec3 pixel) {
 
 void main() {
   vec3 colorBlack = vec3(0.0);
-  vec3 colorParis = vec3(237.0 / 256.0, 61.0 / 256.0, 144.0 / 256.0);
-  vec3 colorCowboy = vec3(242.0 / 256.0, 213.0 / 256.0, 184.0 / 256.0);
-  vec3 colorLilas = vec3(219.0 / 256.0, 184.0 / 256.0, 242.0 / 256.0);
-  vec3 colorStorm = vec3(167.0 / 256.0, 8.0 / 256.0, 248.0 / 256.0);
-  vec3 colorLime = vec3(150.0 / 256.0, 248.0 / 256.0, 8.0 / 256.0);
+  vec3 colorParis = vec3(0.93, 0.24, 0.56);
+  vec3 colorCowboy = vec3(0.96, 0.83, 0.72);
+  vec3 colorLilas = vec3(0.86, 0.72, 0.95);
+  vec3 colorStorm = vec3(0.65, 0.03, 0.97);
+  vec3 colorLime = vec3(0.59, 0.97, 0.03);
 
   float alpha = 1.0;
   vec3 pixel = colorCowboy;
 
   vec2 coord = vec2(gl_FragCoord.xy - 0.5 * uCanvasSize.xy);
   coord = 2.0 * coord.xy / uCanvasSize.y;
-
-  if (mod(coord.x, 0.1) < 0.005) {
-    pixel = colorParis;
-  }
-  if (mod(coord.y, 0.1) < 0.005) {
-    pixel = colorParis;
-  }
 
   gl_FragColor = vec4(pixel, alpha);
 }
