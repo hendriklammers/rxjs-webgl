774adfb7c3ad8a40eaebb5b416a9e90e8993afdf
Hendrik Lammers <lammers@gmail.com>
2016-08-10 23:06:55 +0200

    Update streams

--- src/main.js
+++ src/main.js
@@ -6,50 +6,61 @@ import vertexSource from './shaders/vertex.glsl'
 const dom$ = Observable
   .fromEvent(document, 'DOMContentLoaded')
 
-const render$ = Observable
+const animation$ = Observable
   .interval(1000 / 60, Scheduler.requestAnimationFrame)
-  .map(() => ({time: Date.now(), deltaTime: null}))
+  .map(() => ({time: Date.now(), deltaTime: null, count: 0}))
   .scan((previous, current) => ({
     time: current.time,
-    deltaTime: (current.time - previous.time) / 1000
+    deltaTime: (current.time - previous.time) / 1000,
+    count: previous.count + 1
   }))
 
 const window$ = Observable
   .fromEvent(window, 'resize')
   .debounceTime(250)
+  .distinctUntilChanged()
   .map(event => ({
     width: event.currentTarget.innerWidth,
     height: event.currentTarget.innerHeight
   }))
+  .startWith({
+    width: window.innerWidth,
+    height: window.innerHeight
+  })
+
+const render$ = animation$
+  .withLatestFrom(window$)
+  .map(([time, screen]) => ({
+    time,
+    screen
+  }))
 
 const getCanvas = function() {
   const canvas = document.createElement('canvas')
-  canvas.width = window.innerWidth
-  canvas.height = window.innerHeight
 
-  return canvas
-}
-
-const getBody = function() {
   const body = document.body
   body.style.margin = '0'
   body.style.padding = '0'
   body.style.overflow = 'hidden'
+  body.appendChild(canvas)
 
-  return body
+  window$
+    .subscribe(size => {
+      canvas.width = size.width,
+      canvas.height = size.height
+    })
+
+  return canvas
 }
 
 function init() {
-  const body = getBody(),
-    canvas = getCanvas(),
+  const canvas = getCanvas(),
     gl = getContext(canvas),
     program = createProgram(gl, vertexSource, fragmentSource),
     vertexPosBuffer = createScreenBuffer(gl)
 
-  body.appendChild(canvas)
-
-  program.vertexPosAttrib = gl.getAttribLocation(program, 'aVertexPosition')
-  program.canvasSizeUniform = gl.getUniformLocation(program, 'uCanvasSize')
+  program.vertexPosAttrib = gl.getAttribLocation(program, 'a_vertexPosition')
+  program.canvasSizeUniform = gl.getUniformLocation(program, 'u_resolution')
 
   gl.useProgram(program)
   gl.enableVertexAttribArray(program.vertexPosArray)
@@ -59,9 +70,11 @@ function init() {
                           false, 0, 0)
   gl.uniform2f(program.canvasSizeUniform, canvas.clientWidth, canvas.clientHeight)
 
-  render$.subscribe(render)
+  // render$.subscribe(render)
 
+  render()
   function render() {
+    gl.uniform2f(program.canvasSizeUniform, canvas.clientWidth, canvas.clientHeight)
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, vertexPosBuffer.numItems)
   }
 }
