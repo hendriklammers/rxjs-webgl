071b6a5782c6c68763ced56e71efcbf945bd930d
Hendrik Lammers <lammers@gmail.com>
2016-08-17 21:53:12 +0200

    Add shader for 17/08

---
+++ src/shaders/fragment-17082016.glsl
@@ -0,0 +1,15 @@
+#ifdef GL_ES
+  precision mediump float;
+#endif
+
+uniform vec2 u_resolution;
+uniform float u_time;
+
+void main() {
+  float alpha = 1.0;
+  vec3 pixel = vec3(0.0);
+
+  pixel = vec3(cos((gl_FragCoord.x + u_time * 0.01) * (gl_FragCoord.y + u_time * 0.01)));
+
+  gl_FragColor = vec4(pixel, alpha);
+}
