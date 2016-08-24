61ab6c8901d6052a737f5826620f32071086d46b
Hendrik Lammers <lammers@gmail.com>
2016-08-24 17:54:36 +0200

    Simple grid

--- src/shaders/fragment-grid.glsl
+++ src/shaders/fragment-grid.glsl
@@ -7,9 +7,6 @@ uniform float u_time;
 uniform vec2 u_mouse;
 
 void main() {
-  vec3 colorParis = vec3(237.0 / 256.0, 61.0 / 256.0, 144.0 / 256.0);
-  vec3 colorCowboy = vec3(242.0 / 256.0, 213.0 / 256.0, 184.0 / 256.0);
-  vec3 colorLilas = vec3(219.0 / 256.0, 184.0 / 256.0, 242.0 / 256.0);
   vec3 colorStorm = vec3(167.0 / 256.0, 8.0 / 256.0, 248.0 / 256.0);
   vec3 colorLime = vec3(150.0 / 256.0, 248.0 / 256.0, 8.0 / 256.0);
 
@@ -26,25 +23,21 @@ void main() {
   // 2.0 * -370 -> -740 / 900 => -0.82222
   // 2.0 * -250 -> -500 / 900 => -0.55555
 
-  // 0.1*0.1 square
-  /* if (pos.x > 0.9 && pos.x < 1.0) { */
-  /*   if (pos.y > 0.9 && pos.y < 1.0) { */
-  /*     pixel = colorLime; */
-  /*   } */
-  /* } */
-
+  // 0.1*0.1 grid
   if (mod(pos.x, 0.2) < 0.1 && mod(pos.y, 0.2) < 0.1) {
+    pixel = colorStorm;
+  } else if (mod(pos.x, 0.2) > 0.1 && mod(pos.y, 0.2) > 0.1) {
     pixel = colorLime;
   }
 
-  if (mod(pos.x, 0.2) > 0.1 && mod(pos.y, 0.2) > 0.1) {
-    pixel = colorLime;
-  }
+  // Line every 100px
+  float lineSpace = 2.0 / u_resolution.y * 100.0;
+  float lineWidth = 2.0 / u_resolution.y * 5.0;
 
-  // Circle
-  /* if (length(coord - center) < radius) { */
-  /*   pixel = color; */
-  /* } */
+  if (mod(pos.x, lineSpace) < lineWidth ||
+      mod(pos.y, lineSpace) < lineWidth) {
+    pixel = vec3(1.0);
+  }
 
   gl_FragColor = vec4(pixel, alpha);
 }
